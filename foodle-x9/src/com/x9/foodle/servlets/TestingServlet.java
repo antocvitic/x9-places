fa2a11807bb9d46e878c734df3122e100bf177bc
tgwizard <tgwizard@3eb6119a-d78e-11de-8a94-ab732a2dc7d9>
2010-04-01 22:26:48 +0000

    just reformatted and reorganized, this is how files should be

--- foodle-x9/src/com/x9/foodle/servlets/TestingServlet.java
+++ foodle-x9/src/com/x9/foodle/servlets/TestingServlet.java
@@ -1,95 +1,89 @@
 package com.x9.foodle.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.*;
 
 //import com.mysql.jdbc.Connection;
 
 /**
  * Servlet implementation class for UserTest: HelloServlet
- *
+ * 
  */
- public class TestingServlet extends javax.servlet.http.HttpServlet 
-     implements javax.servlet.Servlet {
+@SuppressWarnings("serial")
+public class TestingServlet extends javax.servlet.http.HttpServlet implements
+		javax.servlet.Servlet {
 
 	public TestingServlet() {
 		super();
-	}   	
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-	    throws ServletException, IOException {
-		
-	
 	}
-	
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-	    throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
 		String Name = request.getParameter("Name");
-		
+
 		response.setContentType("text/html");
-	    ServletOutputStream out = response.getOutputStream();
-	    out.println("<html>");
-	    out.println("<head><title>Hello World</title></head>");
-	    out.println("<body>");
-	    
-	    try {
-	    //out.println("load mysql driver");
-		Class.forName("com.mysql.jdbc.Driver").newInstance();
-	    }
-	    catch (Exception E) {
-		out.println("Unable to load driver.");
-		E.printStackTrace();
-	    }
-	    
-	    out.println("<br><hr>");
-	    
-	    //Get a connection
-	    try {
-		Connection Conn = 
-		    DriverManager.getConnection("jdbc:mysql://localhost/x9db","x9user","x9pwd");
+		ServletOutputStream out = response.getOutputStream();
+		out.println("<html>");
+		out.println("<head><title>Hello World</title></head>");
+		out.println("<body>");
 
-		/*
-		 * A PreparedStatement then execute it and catch result in ResultSet
-		*/	
-		PreparedStatement pStatement = Conn.prepareStatement(
-	    "Select * From Users where FirstName LIKE ? OR LastName Like ? OR UserName LIKE ?");
-		
-		pStatement.setString(1,Name);
-		pStatement.setString(2,Name);
-		pStatement.setString(3,Name);
-
-		ResultSet RS = pStatement.executeQuery();
-		// Show it
-		while (RS.next()) {
-		    out.println("Full name: " + RS.getString(4) +
-		    		" " + RS.getString(5)+ " Age: " + RS.getString(2));
+		try {
+			// out.println("load mysql driver");
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (Exception E) {
+			out.println("Unable to load driver.");
+			E.printStackTrace();
 		}
-		// Clean up after ourselves
-		RS.close();
-		pStatement.close();
-		Conn.close();
-	    }
-	    catch (SQLException E) {
-		out.println("SQLException: " + E.getMessage());
-		out.println("SQLState:     " + E.getSQLState());
-		out.println("VendorError:  " + E.getErrorCode());
-	    }
-	    out.println("<br><hr>");
-	    
-	    out.println("</body></html>");
-	}  
+
+		out.println("<br><hr>");
+
+		// Get a connection
+		try {
+			Connection Conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost/x9db", "x9user", "x9pwd");
+
+			/*
+			 * A PreparedStatement then execute it and catch result in ResultSet
+			 */
+			PreparedStatement pStatement = Conn
+					.prepareStatement("Select * From Users where FirstName LIKE ? OR LastName Like ? OR UserName LIKE ?");
+
+			pStatement.setString(1, Name);
+			pStatement.setString(2, Name);
+			pStatement.setString(3, Name);
+
+			ResultSet RS = pStatement.executeQuery();
+			// Show it
+			while (RS.next()) {
+				out.println("Full name: " + RS.getString(4) + " "
+						+ RS.getString(5) + " Age: " + RS.getString(2));
+			}
+			// Clean up after ourselves
+			RS.close();
+			pStatement.close();
+			Conn.close();
+		} catch (SQLException E) {
+			out.println("SQLException: " + E.getMessage());
+			out.println("SQLState:     " + E.getSQLState());
+			out.println("VendorError:  " + E.getErrorCode());
+		}
+		out.println("<br><hr>");
+
+		out.println("</body></html>");
+	}
 }
-	
-
-
-
-
-
