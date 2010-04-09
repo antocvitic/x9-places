ec72d981667e531d62902e143fbc0327cba1c60e
tgwizard <tgwizard@3eb6119a-d78e-11de-8a94-ab732a2dc7d9>
2010-04-09 00:30:48 +0000

    * controllers for login & logout (UNTESTED...) * small utility function to easily get an MD5 hash of a string * function to get the currently logged in user (UNTESTED...)

---
+++ foodle-x9/src/com/x9/foodle/user/LogoutController.java
@@ -0,0 +1,28 @@
+package com.x9.foodle.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@SuppressWarnings("serial")
+public class LogoutController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String redirect = req.getParameter("redirect");
+		HttpSession session = req.getSession(false);
+
+		if (session != null) {
+			session
+					.removeAttribute(LoginController.LOGGED_IN_SESSION_ATTRIBUTE);
+		}
+
+		resp.sendRedirect(redirect);
+	}
+
+}
