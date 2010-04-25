8c7c1593c30ea8c00084d495e7e979e72f002e8b
shazmodan <shazmodan@3eb6119a-d78e-11de-8a94-ab732a2dc7d9>
2010-04-25 15:23:47 +0000

    Update of ReviewModel, need to do Solr Stuff

--- foodle-x9/src/com/x9/foodle/review/ReviewModel.java
+++ foodle-x9/src/com/x9/foodle/review/ReviewModel.java
@@ -1,7 +1,57 @@
 package com.x9.foodle.review;
 
-//William testing comment, remove this.
+import java.util.Date;
+
+/*
+ * TOOD: constructor and solr stuff 
+ */
 
 public class ReviewModel {
-
+	
+	private String reviewID;
+	private String text;
+	private Date timeAdded;
+	private String venueID;
+	private String creator;
+	private String rankValue;
+	
+	
+	public String getReviewID() {
+		return reviewID;
+	}
+	public void setReviewID(String reviewID) {
+		this.reviewID = reviewID;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getTimeAdded() {
+		return timeAdded;
+	}
+	public void setTimeAdded(Date timeAdded) {
+		this.timeAdded = timeAdded;
+	}
+	public String getVenueID() {
+		return venueID;
+	}
+	public void setVenueID(String venueID) {
+		this.venueID = venueID;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public String getRankValue() {
+		return rankValue;
+	}
+	public void setRankValue(String rankValue) {
+		this.rankValue = rankValue;
+	}
 }
+
+
