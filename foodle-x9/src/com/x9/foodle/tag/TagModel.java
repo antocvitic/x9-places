2b8857751d2f228cf5c6986f4f12e9a2cc2a3948
remi.ta75 <remi.ta75@3eb6119a-d78e-11de-8a94-ab732a2dc7d9>
2010-04-28 16:41:02 +0000

    TagModel I've implemented a bit TagModel, and created EditController.java, which will be use to test the tags in the venue (see tagmodel_test.jsp and VenueModel.java) TagController is useless for the moment

--- foodle-x9/src/com/x9/foodle/tag/TagModel.java
+++ foodle-x9/src/com/x9/foodle/tag/TagModel.java
@@ -2,6 +2,7 @@ package com.x9.foodle.tag;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Vector;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
@@ -28,206 +29,41 @@ import com.x9.foodle.venue.exceptions.InvalidVenueException;
 
 public class TagModel {
 
-	public static final String SOLR_TYPE = "tagmodel";
-
-	private String venueID;
-	private int userID;
-	private String tagText;
-
-	public TagModel(){
-		venueID = null;
-		userID = -1;
-		tagText = null;
+	
+	//TODO ?
+	public void getFromSolr(String venueID){
+		
 	}
 	
-	private TagModel(TagModel copyMe) {
-		this();
-		copy(this, copyMe);
-	}
-
-	public TagModel getFromSolr(String venueID){
+	public SolrDocumentList getVenueFromSolr(Vector<String> tags){
 		try {
 			SolrServer server = SolrUtils.getSolrServer();
 			SolrQuery query = new SolrQuery();
 			// TODO: make the query safe
-			query.setQuery("venueID:" + venueID + " AND type:" + SOLR_TYPE);
+			String queryString="";
+			for(int i=0; i<tags.size(); i++){
+				if(i!=tags.size()-1){
+					queryString+="tag:"+tags.get(i)+" AND";
+				}
+				else{
+					queryString+="tag:"+tags.get(i);
+				}
+			}
+			
+			query.setQuery(queryString);
 			QueryResponse rsp = server.query(query);
 			SolrDocumentList docs = rsp.getResults();
+			
 			if (docs.isEmpty()) {
-				// no tag corresponding to the venueID found
 				return null;
 			}
 
-			/*if (docs.size() > 1) {
-				throw new SolrRuntimeException(
-						"Too many results in TagModel.getFromSolr: "
-						+ docs.size());
-			}*/
-
-			SolrDocument doc = docs.get(0);
-
-			TagModel tag = tagFromSolrDocument(doc); //Should it return a list of tags ?
-			return tag;
+			return docs;
 
 		} catch (SolrServerException e) {
 			throw new SolrRuntimeException("Solr Server Exception", e);
 		}		
 	}
-	
-	//TODO
-	public void createNew(String venueID){
-		
-	}
-	
-	//TODO
-	private TagModel tagFromSolrDocument(SolrDocument doc) {
-		return null;
-	}
-	
-	//TODO
-	public static class Builder {
-		
-		private TagModel tag;
-		private TagModel editMe;
-		
-		public Builder() {
-			this.tag = new TagModel();
-		}
-		
-		private Builder(TagModel editMe) {
-			this.tag = new TagModel(editMe);
-			this.editMe = editMe;
-		}
-
-		
-		
-		
-		/**
-		 * Will throw a subclass of {@link InvalidVenueException} if any of the
-		 * parameters for this venue is invalid.
-		 * 
-		 * @throws InvalidUserIDException
-		 * @throws InvalidVenueIDException
-		 * @throws InvalidTagTextException
-		 */
-		
-		/*public void validate() throws InvalidUserIDException, 
-		 InvalidVenueIDException, 
-		 InvalidTagTextException {
-			
-			Validator.validate(tag);
-		}*/
-
-		/**
-		 * Calls {@code validate()} to check the parameters, and upon success
-		 * inserts them as a venu in Solr. If the builder was created using the
-		 * default constructor, a new venue is inserted. If it was created using
-		 * the {@link VenueModel#getEditable()} method, that venue will be
-		 * updated, and all references to that object will be updated.
-		 * 
-		 * Will throw subclasses of {@link InvalidVenueException} if any
-		 * parameter is invalid.
-		 * 
-		 * A venue model will be returned if no exceptions are thrown.
-		 * 
-		 * @return the inserted or edited venue model, never null
-		 * @throws InvalidIDException
-		 * @throws InvalidTitleException
-		 * @throws InvalidAddressException
-		 * @throws InvalidDescriptionException
-		 * @throws InvalidNumberOfRatingsException
-		 * @throws InvalidAverageRatingException
-		 * @throws InvalidCreatorIDException
-		 * @throws SolrRuntimeException
-		 */
-		/*public VenueModel apply() throws InvalidIDException,
-				InvalidTitleException, InvalidAddressException,
-				InvalidDescriptionException, InvalidNumberOfRatingsException,
-				InvalidAverageRatingException, InvalidCreatorIDException,
-				SolrRuntimeException {
-			if (editMe == null) {
-				venue.timeAdded = DateUtils.getNowUTC();
-			}
-			venue.lastUpdated = DateUtils.getNowUTC();
-
-			// throws on bad data
-			validate();
-
-			try {
-				SolrServer server = SolrUtils.getSolrServer();
-				SolrInputDocument doc = new SolrInputDocument();
-
-				doc.addField("id", venue.id);
-				doc.addField("type", SOLR_TYPE);
-				doc.addField("title", venue.title);
-				doc.addField("address", venue.address);
-				doc.addField("description", venue.description);
-				// doc.addField("photos", venue.photos);
-				doc.addField("numberOfRatings", venue.numberOfRatings);
-				doc.addField("averageRating", venue.averageRating);
-				doc.addField("closed", venue.closed);
-				doc.addField("timeAdded", DateUtils
-						.dateToSolrDate(venue.timeAdded));
-				doc.addField("creator", venue.creatorID);
-				doc.addField("lastModified", DateUtils
-						.dateToSolrDate(venue.lastUpdated));
-
-				server.add(doc);
-				server.commit();
-
-				if (editMe != null) {
-					copy(editMe, venue);
-					return editMe;
-				} else {
-					return venue;
-				}
-
-			} catch (SolrServerException e) {
-				throw new SolrRuntimeException("Solr Server Exception", e);
-			} catch (IOException e) {
-				throw new SolrRuntimeException("Solr IOException", e);
-			}
-		}*/
-
-		
-		
-
-	}
-	
-	//TODO
-	public static class Validator {
-		
-	}
-
-	public String getVenueID() {
-		return venueID;
-	}
-
-	public int getUserID() {
-		return userID;
-	}
-
-	public String getTagText() {
-		return tagText;
-	}
-
-	public static String getSolrType() {
-		return SOLR_TYPE;
-	}
-
-	private void copy(TagModel tagModel, TagModel copyMe) {
-		tagModel.tagText = copyMe.tagText;
-		tagModel.userID = copyMe.userID;
-		tagModel.venueID = copyMe.venueID;
-		
-	}
-
-	public String toString() {
-		return "TagModel [venueID=" + venueID + "," +
-		"tagText=" + tagText + ", userID="
-		+ userID+"]";
-	}
-
 
 
 }
